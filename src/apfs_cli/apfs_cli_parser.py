3ac29dbd6c0711ba0f331b24e3f37dbd4e72c32d
mbasala <mbasala@radford.edu>
2024-03-12 17:10:21 -0400

    Mostly stub classes with some info, but structure is done now and a plan has bones now

--- src/apfs_cli/apfs_cli_parser.py
+++ src/apfs_cli/apfs_cli_parser.py
@@ -1,11 +1,14 @@
 import argparse
+from apfs_api.utils import DATA_DICT_COLUMNS
 
 
-class apfs_argparser():
+class ApfsArgParser():
     parser: argparse.ArgumentParser
 
     def __init__(self):
         self.parser = self.init_parser()
+        self.parse_args()
+        self.add_filter_args()
 
     def init_parser(self) -> argparse.ArgumentParser:
         return argparse.ArgumentParser(
@@ -15,8 +18,35 @@ class apfs_argparser():
 
     def parse_args(self):
         self.parser.add_argument(
-            "--color",
-            action=ChoicesAction,
-            nargs="+",
-            action_values=["red", "blue", "green"],
+            "--field", "-c",
+            action="extend",
+            nargs="*",
+            choices=DATA_DICT_COLUMNS,
         )
+
+        self.parser.add_argument(
+            "--sort", "-s",
+            action="store",
+            nargs=1,
+            choices=DATA_DICT_COLUMNS,
+        )
+
+    def add_filter_args(self):
+        # Here be dragons
+        for flag in DATA_DICT_COLUMNS:
+            self.parser.add_argument(
+                f"--{flag}",
+                action="store",
+                help=f"Filter the results by {flag} specified",
+                nargs='?',
+            )
+
+
+def apfs_cli_wrapper():
+    parser = ApfsArgParser().parser
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    apfs_cli_wrapper()
