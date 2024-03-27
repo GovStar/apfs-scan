f4c7810979064a3aa287e8059ef3f2db3ffd2e7d
mbasala <mbasala@radford.edu>
2024-03-27 11:13:15 -0400

    Code is bork still right now.

--- apfs_scan/apfs_api/apfs_wayback_api_hooks.py
+++ apfs_scan/apfs_api/apfs_wayback_api_hooks.py
@@ -5,7 +5,9 @@ from typing import List, Dict, Optional, Any
 import requests
 from pandas import DataFrame, read_json
 from os.path import exists
+from gzip import GzipFile, decompress
 from requests import Session
+from io import BytesIO
 from apfs_scan.apfs_api.utils import exception_log_and_exit
 
 
@@ -14,13 +16,18 @@ class WayBackApfsSession:
     WAYBACK_API_ENDPOINT: str = 'https://archive.org/wayback/available'
 
     session: requests.Session
-    home_page: requests.Response
-    payload: Dict[str, str] = {"url": APFS_URL}
+    wayback_response: requests.Response
+    payload: Dict[str, str] = {"url": APFS_URL,
+                               "output": "json",
+                               }
     header: Dict[str, str] = {
         "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'}
-    forcast_records_json: dict[str, Any]
     forcast_history_json: dict[str, Any]
     apfs_file_path: str
+    apfs_gzip_response: bytes
+    max_tries: int = 3
+    # 5 second cooldown between api calls, to prevent getting blocked
+    api_call_cooldown: int = 5
     forcast_records_df: DataFrame
     apfs_file_exist: bool
     response: Optional[requests.Response] = None
@@ -29,26 +36,25 @@ class WayBackApfsSession:
     logger: logging.Logger
     no_connect: bool = False
 
-    def __init__(self, apfs_file_path: str):
+    def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.apfs_file_path = apfs_file_path
-        self.validate_output_file()
-        if not self.no_connect:
-            self.setup_apfs_connection()
-        self.write_apfs_data()
+        self.api_apfs_call()
+        self.populate_data_history()
         self.update_data_dict_columns()
 
-    def setup_apfs_connection(self) -> None:
+    def api_apfs_call(self) -> None:
         try:
-            user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
             self.session = Session()
             self.session.headers.update(self.header)
-            self.forcast_history_json = self.session.get(url=self.WAYBACK_API_ENDPOINT, params=self.payload).json()
-            if self.home_page.status_code != 200:
+            self.session.params = self.payload
+            self.session.adapters.DEFAULT_RETRIES = self.max_tries
+            self.wayback_response = self.session.get(url=self.WAYBACK_API_ENDPOINT, params=self.payload)
+            self.forcast_history_json = self.wayback_response.json()
+            if self.wayback_response.status_code != 200:
                 self.logger.error("WayBack connection returned a bad status code")
                 raise requests.HTTPError("WayBack connection returned a bad status code")
             else:
-                self.logger.debug("APFS connection good status code: " + str(self.home_page.status_code))
+                self.logger.debug("APFS connection good status code: " + str(self.wayback_response.status_code))
         except requests.HTTPError as e:
             self.logger.error("WayBack connection returned a bad status code")
             exception_log_and_exit(e)
@@ -61,9 +67,7 @@ class WayBackApfsSession:
 
     def populate_data_history(self) -> None:
         try:
-            self.forcast_records_json = self.session.get(
-                url=self.forcast_history_json['archived_snapshots']['closest']['url']).json()
-            self.forcast_records_df = DataFrame.from_dict(self.forcast_records_json)
+            self.forcast_records_df = DataFrame.from_dict(self.forcast_history_json['archived_snapshots']['closest']['url'])
         except requests.HTTPError as e:
             self.logger.error("WayBack connection returned a bad status code")
             exception_log_and_exit(e)
@@ -90,8 +94,8 @@ class WayBackApfsSession:
     def validate_apfs_data(self) -> None:
         try:
             # Will throw an error if not a valid type this works as type validating
-            json.loads(json.dumps(self.forcast_records_json))
-            if len(self.forcast_records_json) < 1:
+            json.loads(json.dumps(self.forcast_history_json))
+            if len(self.forcast_history_json) < 1:
                 self.logger.error("APFS returned no data")
             else:
                 self.logger.debug("APFS returned data")
@@ -100,62 +104,6 @@ class WayBackApfsSession:
             exception_log_and_exit(e)
         except Exception as e:
             self.logger.error("APFS validate_apfs_data unhandled error: " + str(e))
-            exception_log_and_exit(e)
-
-    def update_ofd_data(self) -> None:
-        if self.data_ofd:
-            self.logger.debug("APFS data is of date")
-            self.forcast_records_json = self.get_apfs_data()
-            self.update_data_dict_columns()
-            self.data_ofd = False
-            self.logger.info("APFS data updated")
-        else:
-            self.logger.debug("APFS data is not of date")
-
-    def validate_output_file(self) -> None:
-        self.logger.debug(self.apfs_file_path)
-        try:
-            # Checks if file exists and is writeable
-            access(self.apfs_file_path, W_OK)
-            self.apfs_file_exist = exists(self.apfs_file_path)
-        except FileNotFoundError as e:
-            self.logger.error("APFS file path is not writeable")
-            exception_log_and_exit(e)
-        except PermissionError as e:
-            self.logger.error("APFS file path does not have write permissions")
-            exception_log_and_exit(e)
-        except Exception as e:
-            self.logger.error("APFS validate_output_file unhandled error: " + str(e))
-            exception_log_and_exit(e)
-
-    def write_apfs_data(self) -> None:
-        try:
-            with open(self.apfs_file_path, 'w', encoding='utf-8') as outfile:
-                self.logger.debug("Writing apfs forcast data to file")
-                DataFrame.to_json(self.forcast_records_df, outfile)
-        except ValueError as e:
-            self.logger.error("APFS returned a bad type when writing to file")
-            exception_log_and_exit(e)
-        except FileNotFoundError as e:
-            self.logger.error("APFS file path does not exist")
-            exception_log_and_exit(e)
-        except Exception as e:
-            self.logger.error("APFS write_apfs_data unhandled error: " + str(e))
-            exception_log_and_exit(e)
-
-    def read_apfs_data(self) -> None:
-        try:
-            with open(self.apfs_file_path, 'r', encoding='utf-8') as outfile:
-                self.logger.debug("Reading apfs forcast from file: " + self.apfs_file_path)
-                self.forcast_records_df = read_json(outfile)
-        except ValueError as e:
-            self.logger.error("APFS returned a bad type when writing to file")
-            exception_log_and_exit(e)
-        except FileNotFoundError as e:
-            self.logger.error("APFS file path does not exist")
-            exception_log_and_exit(e)
-        except Exception as e:
-            self.logger.error("APFS write_apfs_data unhandled error: " + str(e))
             exception_log_and_exit(e)
 
     def pull_historic_data(self) -> None:
@@ -169,4 +117,14 @@ class WayBackApfsSession:
             exception_log_and_exit(e)
         except Exception as e:
             self.logger.error("APFS pull_historic_data unhandled error: " + str(e))
+            exception_log_and_exit(e)
+
+    def decompress_apfs_response(self) -> None:
+        try:
+            self.forcast_history_json = decompress(self.apfs_gzip_response)
+        except ValueError as e:
+            self.logger.error("APFS returned a bad type when decompressing")
+            exception_log_and_exit(e)
+        except Exception as e:
+            self.logger.error("APFS decompress_apfs_response unhandled error: " + str(e))
             exception_log_and_exit(e)
--- apfs_scan/apfs_cli/apfs_cli_parser.py
+++ apfs_scan/apfs_cli/apfs_cli_parser.py
@@ -1,8 +1,11 @@
 import argparse
 import json
+from typing import Any
+
 from apfs_scan.apfs_api.utils import DATA_DICT_COLUMNS, exception_log_and_exit
 from apfs_scan.apfs_api.apfs_cloud_api_hooks import ApfsSession
 from apfs_scan.apfs_api.forecast_parser import filter_on_field, ApfsForecastParser
+from apfs_scan.apfs_cli.prettyprintformate import pretty_print_forecast_data
 from os import getcwd
 from os.path import abspath, exists, dirname
 import logging
@@ -35,7 +38,7 @@ class TestingFileAction(argparse.Action):
                 logging.error("File path does not exist")
                 raise FileNotFoundError
         except FileNotFoundError as e:
-            create_outfile(file_path)
+            exception_log_and_exit(e)
         except Exception as e:
             exception_log_and_exit(e)
 
@@ -103,39 +106,26 @@ class ApfsArgParser:
 def apfs_cli_wrapper():
     logger = logging.getLogger(__name__)
 
-    apfs_cli = ApfsArgParser()
+    apfs_cli: ApfsArgParser = ApfsArgParser()
     logger.debug('Created cli parser')
     logger.debug("CWD: " + str(getcwd()))
-    args = vars(apfs_cli.parser.parse_args())
+    args: dict[str, Any] = vars(apfs_cli.parser.parse_args())
     logger.debug('apfs_cli args: ' + str(args))
+    logger.debug('Input path: ' + str(args['forcast_records']))
 
-    apfs_handler = ApfsForecastParser(apfs_file_path=args['forcast-records'])
+    apfs_handler = ApfsForecastParser(apfs_file_path=args['forcast_records'][0])
 
-    apfs_api = ApfsSession(apfs_handler.data_ofd)
-
-    logger.debug('Created cli parser')
+    if apfs_handler.data_ofd:
+        logger.debug('Pulling new data from API')
+        apfs_api = ApfsSession()
+        apfs_handler.apfs_data = apfs_api.forcast_records_df.copy()
 
     # Needs to be changed to an accumulator that then does the filtering all at once.
     for flag in apfs_api.DATA_DICT_COLUMNS:
         if args[flag] is not None:
             apfs_api.forcast_records_df = filter_on_field(apfs_api.forcast_records_df, field=flag, value=args[flag])
 
-    print(str(apfs_api.forcast_records_df))
-
-
-def validate_output_file(file_path: str, logger: logging.Logger):
-    try:
-        if not exists(file_path):
-            logger.error("File path does not exist")
-            raise FileNotFoundError
-    except FileNotFoundError as e:
-        create_outfile(file_path, logger)
-    except Exception as e:
-        exception_log_and_exit(e)
-
-
-def create_outfile(file_path, logger: logging.Logger):
-    return True
+    pretty_print_forecast_data(data=apfs_api.forcast_records_df.copy())
 
 
 if __name__ == '__main__':
