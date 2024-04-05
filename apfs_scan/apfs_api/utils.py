017054b05ba2efc0212f7e9cd55507cd54f48428
mbasala <mbasala@radford.edu>
2024-04-05 13:48:30 -0400

    Small changes and linux deamon stub, added some more API stubs, and JSON fields in utils

--- apfs_scan/apfs_api/utils.py
+++ apfs_scan/apfs_api/utils.py
@@ -1,4 +1,6 @@
 # Last known data columns
+from typing import Dict, Any
+
 DATA_DICT_COLUMNS: list = ['id', 'mission', 'organization', 'small_business_program',
                            'dollar_range', 'contract_type', 'contract_vehicle', 'competitive', 'award_quarter',
                            'estimated_release_date', 'small_business_set_aside', 'publish_date', 'previous_publish_date',
@@ -15,6 +17,61 @@ DATA_DICT_COLUMNS: list = ['id', 'mission', 'organization', 'small_business_prog
                            'apfs_coordinator_office', 'current_state', 'last_updated_date',
                            'published_date', 'previous_published_date']
 
+SAME_opportunity_pointOfContact_json: Dict[str, Any] = {'Fax': '', 'Type': '', 'Email': '', 'Phone': '', 'Title': '',
+                                                        'Full name': '', 'additionalInfo': Dict[str, Any]}
+SAM_opportunity_country_json: Dict[str, Any] = {'code': '', 'name': ''}
+SAM_opportunity_state_json: Dict[str, Any] = {'code': '', 'name': ''}
+SAM_opportunity_city_json: Dict[str, Any] = {'code': '', 'name': '', 'state': '', 'stateCode': ''}
+SAM_opportunity_location_json: Dict[str, Any] = {'streetAddress': '', 'streetAddress2': '',
+                                                 'city': SAM_opportunity_city_json,
+                                                 'country': SAM_opportunity_country_json, 'zip': ''}
+SAM_opportunity_awardee_json: Dict[str, Any] = {'name': '', 'ueiSAM': '', 'location': SAM_opportunity_location_json}
+SAM_opportunity_award_json: Dict[str, Any] = {'number': 'str', 'amount': int, 'date': '',
+                                              'awardee': SAM_opportunity_awardee_json}
+SAM_opportunity_officeAddress_json: Dict[str, Any] = {'city': '', 'state': '', 'zip': ''}
+SAM_opportunity_placeOfPerformance_json: Dict[str, Any] = {'streetAddress': '', 'streetAddress2': '',
+                                                           'city': SAM_opportunity_city_json,
+                                                           'state': SAM_opportunity_state_json,
+                                                           'country': SAM_opportunity_country_json, 'zip': ''}
+
+SAM_opportunity_request_params: Dict[str, Any] = {'ptype': '', 'solnum': '', 'noticeid': '', 'title': '',
+                                                  'postedFrom': '', 'postedTo': '', 'deptname': '', 'subtier': '',
+                                                  'state': '', 'status (Coming Soon)': '', 'zip': '',
+                                                  'organizationCode': '', 'organizationName': '', 'typeOfSetAside': '',
+                                                  'typeOfSetAsideDescription': '', 'ncode': '', 'ccode': '',
+                                                  'rdlfrom': '',
+                                                  'rdlto': '', 'limit': int, 'offset': int}
+SAM_opportunity_response_params: Dict[str, Any] = {'totalRecords': int, 'limit': int, 'offset': int, 'title': '',
+                                                   'solicitationNumber': '', 'fullParentPathName': '',
+                                                   'fullParentPathCode': '',
+                                                   'department': '', 'subtier': '', 'office': '', 'postedDate': '',
+                                                   'type': '', 'baseType': '', 'archiveType': '', 'archiveDate': '',
+                                                   'setAside': '', 'setAsideCode': '',
+                                                   'responseDeadLine': '', 'naics': '', 'classificationCode': '',
+                                                   'active': '', 'data.award': SAM_opportunity_award_json,
+                                                   'pointofContact': SAME_opportunity_pointOfContact_json,
+                                                   'description': '',
+                                                   'organizationType': '',
+                                                   'officeAddress': SAM_opportunity_officeAddress_json,
+                                                   'placeOfPerformance': SAM_opportunity_placeOfPerformance_json,
+                                                   'additionalInfoLink': ' ', 'uiLink': '', 'links': [str],
+                                                   'resourceLinks': [str]
+                                                   }
+
+SAM_cityLookUp_county_json: Dict[str, Any] = {'country': '', 'code2': '', 'code': ''}
+SAM_cityLookUp_state_json: Dict[str, Any] = {'statecode': '', 'state': ''}
+SAM_cityLookUp_request_json: Dict[str, Any] = {'api_key': '', 'cc': '', 'searchby': '', 'searchvalue' : '', 'q': '', 'active': '', 'citycode': ''}
+SAM_cityLookUp_response_json: Dict[str, Any] = {'cityCode': '', 'city': '', 'state': '', 'searchvalue' : '', 'q': '', 'active': '', 'citycode': ''}
+
+SAM_countyLookUp_request_json: Dict[str, Any] = {'api_key': '', 'searchby': '', 'searchvalue': '', 'q': '', 'active': ''}
+SAM_countyLookUp_response_json: Dict[str, Any] = {'country': '', 'countryFullName': '', 'countryCode2': '', 'countrycode': '', 'href': ''}
+
+SAM_stateLookUp_county_json: Dict[str, Any] = {'country': '', 'countryFullName': '', 'countryCode2': '', 'countrycode': ''}
+SAM_stateLookUp_request_json: Dict[str, Any] = {'api_key': '', 'cc': '', 'searchby': '', 'q': '', 'active': ''}
+SAM_stateLookUp_response_json: Dict[str, Any] = {'statecode': '', 'state': '', 'stateType': '', 'country': SAM_stateLookUp_county_json, 'href': ''}
+
+SAM_zipLookUp_request_json: Dict[str, Any] = {'api_key': '', 'zip': '', 'citycode': '', 'countycode': '', 'statecode': ''}
+SAM_zipLookUp_response_json: Dict[str, Any] = {'zipCode': '', 'description': ''}
 
 def exception_log_and_exit(exception: Exception):
     import logging
